9d1f58eeb555da47a5849d0c4f78067d5d65da34
Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2022-11-25 00:02:09 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Easy Manage music on your server</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
