282c467e7705f4a59c55ea93345dac3fb925b3c1
Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2023-07-29 02:43:28 +0200

    Garden Radio

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Easy Manage music on your server</title>
+        <title>😎 Play Garden Radio Music in Discord</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -17,10 +17,9 @@ export default function Home() {
           To see all commands, use <b>/help</b> and select Manager
         </p>
         <div className="invite">
-          <h2><a href="https://discord.com/oauth2/authorize?client_id=919940620456443935&scope=bot&permissions=2150845440">Invite Bot</a></h2>
+          <h2><a href="https://discord.com/oauth2/authorize?client_id=1075549290279219205&scope=bot&permissions=549775919168">Invite Bot</a></h2>
         </div>
       </main>
-
       <Footer />
     </div>
   )
